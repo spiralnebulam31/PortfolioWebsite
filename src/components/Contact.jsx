2481087a5f2321689f159a52d2918d487668423e
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-11-14 23:16:40 +0000

    Reorganised Contact code (added two new componets to help make the code clearer) and changed the responsiveness (one column on mobile, two on large screens)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ import { linkedin, linkedin2, github, github2, email, email2 } from "../assets";
 import { mountains, planet1, planet2, starrySky1 } from "../assets";
 import Parallax from "./Parallax";
 
+// ... (imports)
+
 const Contact = ({
   linkedinIsHovered,
   handleLinkedinMouseEnter,
@@ -18,7 +20,6 @@ const Contact = ({
   handleEmailMouseEnter,
   handleEmailMouseLeave,
 }) => {
-  // For emailjs
   const form = useRef();
 
   const openPopup = () => {
@@ -48,161 +49,129 @@ const Contact = ({
   };
 
   return (
-<section id="contact">
-
-      <div className="bg-gradient-to-b from-orange-700 to-purple-800 relative bg-cover w-full left-0 right-0 bottom-0 top-0 py-10"> 
-
-      {/* Title */}
+    <section id="contact">
+      <div className="bg-gradient-to-b from-orange-700 to-purple-800 relative bg-cover w-full left-0 right-0 bottom-0 top-0 py-10">
+        {/* Title */}
         <div className="mx-auto px-5">
           <div>
             <p className={styles.sectionSubText + styles.paddingX}>Ways to</p>
-            <h2 className={styles.sectionHeadText + styles.paddingX}>
-              Contact Me
-            </h2>
+            <h2 className={styles.sectionHeadText + styles.paddingX}>Contact Me</h2>
           </div>
         </div>
-      {/* End of title */}
+        {/* End of title */}
 
-
-          {/* Content text */}
-              <div
-                className="text-white font-body text-lg max-w-full sm:px-16 px-6 py-8 leading-[30px]"
-              >
-                <p className="mt-4">Feel free to reach out to me:</p>
-              </div>
-
-                {/* Contact info */}
-                <div
-                  className="bg-gradient-to-b from-cyan-500 to-purple-300 p-[2px] rounded-lg shadow-card text-center"
-                >
-                  <div className="bg-background1 w-full rounded-lg border pb-3 pt-3">
-
-                    {/* Contact links */}
-                    <div className="flex justify-center items-center gap-5 pb-3">
-                      <a
-                        href="mailto:anastasiaadamoudi@gmail.com"
-                        onMouseEnter={handleEmailMouseEnter}
-                        onMouseLeave={handleEmailMouseLeave}
-                        alt="email address"
-                        className="text-secondary hover:text-primary flex items-center whitespace-nowrap
-                        text-sm sm:text-sm md:text-md lg:text-md xl:text-xl"
-                      >
-                        <img
-                          src={emailIsHovered ? email2 : email}
-                          alt="email icon"
-                          className="w-[20px] h-[20px] object-contain"
-                        />
-                        <p className="ml-2">anastasiaadamoudi@gmail.com</p>
-                      </a>
-                    </div>
-
-                    <div className="flex justify-center items-center gap-5 pb-3">
-                      <a
-                        href="https://github.com/AnastasiaAdamoudi"
-                        target="_blank"
-                        rel="noreferrer"
-                        onMouseEnter={handleGithubMouseEnter}
-                        onMouseLeave={handleGithubMouseLeave}
-                        alt="GitHub profile"
-                        className="text-secondary hover:text-primary flex items-center whitespace-nowrap
-                        text-sm sm:text-sm md:text-md lg:text-md xl:text-xl"
-                      >
-                        <img
-                          src={githubIsHovered ? github2 : github}
-                          alt="GitHub icon"
-                          className="w-[20px] h-[20px] object-contain"
-                        />
-                        <p className="ml-2">github.com/AnastasiaAdamoudi</p>
-                      </a>
-                    </div>
-
-                    <div className="flex justify-center items-center gap-5 pb-1">
-                      <a
-                        href="https://www.linkedin.com/in/anastasiaadamoudi89/"
-                        target="_blank"
-                        rel="noreferrer"
-                        onMouseEnter={handleLinkedinMouseEnter}
-                        onMouseLeave={handleLinkedinMouseLeave}
-                        alt="LinkedIn profile"
-                        className="text-secondary hover:text-primary flex items-center whitespace-nowrap
-                        text-sm sm:text-sm md:text-md lg:text-md xl:text-xl"
-                      >
-                        <img
-                          src={linkedinIsHovered ? linkedin2 : linkedin}
-                          alt="LinkedIn icon"
-                          className="w-[20px] h-[20px] object-contain"
-                        />
-                        <p className="ml-2">
-                          linkedin.com/in/anastasiaadamoudi89
-                        </p>
-                      </a>
-                    </div> {/* End of contact links */}
-
-                  </div> {/* End of contact info */}
-                </div> {/* End of contact info container */}
-
-            {/* Contact form */}
-            <div
-              className="text-center w-[60%] md:w-[40%] mx-auto"
-            >
-              <p
-                className={
-                  "text-white font-body text-lg max-w-lg xl:max-w-6xl pb-5"
-                }
-              >
-                Or send me a message below:
-              </p>
-
-              <form ref={form} onSubmit={sendEmail}>
-                <div
-                  className="mb-4 flex items-center
-                   bg-gradient-to-b from-cyan-500 to-purple-300 p-[2px] rounded-lg shadow-card"
-                >
-                  <input
-                    type="text"
-                    placeholder="Your name"
-                    name="from_name"
-                    className="border py-2 px-3 text-body bg-background1 w-full md:w-full rounded-lg text-white text-body"
-                  />
-                </div>
-
-                <div
-                  className="mb-4 flex items-center
-                   bg-gradient-to-b from-cyan-500 to-purple-300 p-[2px] rounded-lg shadow-card"
-                >
-                  <input
-                    type="email"
-                    placeholder="Your email address"
-                    name="user_email"
-                    className="border py-2 px-3 text-body bg-background1 w-full md:w-full rounded-lg text-white text-body"
-                  />
-                </div>
-
-                <div
-                  className="mb-4 flex items-center
-                   bg-gradient-to-b from-cyan-500 to-purple-300 p-[2px] rounded-lg shadow-card"
-                >
-                  <textarea
-                    name="message"
-                    placeholder="Your message"
-                    className="border py-2 px-3 text-body bg-background1 w-full md:w-full rounded-lg text-white text-body"
-                  />
-                </div>
-                <div className="mb-4">
-                  <button
-                    type="submit"
-                    className="bg-secondary hover:bg-primary mt-1 font-links font-bold uppercase
-                            text-sm sm:text-md md:text-l lg:text-xl text-background1 py-2 px-4 rounded-md items-center mx-auto"
-                  >
-                    Send
-                  </button>
-                </div>
-              </form>
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4">
+          {/* Contact Information */}
+          <div className="mb-2 text-center max-w-[80%] xl:max-w-[60%] mx-auto">
+            <div className="text-white font-body text-lg max-w-full sm:px-16 px-6 py-8 leading-[30px]">
+              <p className="mt-4">Feel free to reach out to me:</p>
             </div>
 
+            {/* Contact Info Container */}
+            <div className="bg-gradient-to-b from-cyan-500 to-purple-300 p-[2px] rounded-lg shadow-card text-center">
+              <div className="bg-background1 w-full rounded-lg border pb-3 pt-3">
+
+                {/* Contact Links */}
+                <div className="flex flex-col gap-5 pb-3">
+                  <ContactLink
+                    href="mailto:anastasiaadamoudi@gmail.com"
+                    onMouseEnter={handleEmailMouseEnter}
+                    onMouseLeave={handleEmailMouseLeave}
+                    icon={emailIsHovered ? email2 : email}
+                    text="anastasiaadamoudi@gmail.com"
+                  />
+                  <ContactLink
+                    href="https://github.com/AnastasiaAdamoudi"
+                    target="_blank"
+                    rel="noreferrer"
+                    onMouseEnter={handleGithubMouseEnter}
+                    onMouseLeave={handleGithubMouseLeave}
+                    icon={githubIsHovered ? github2 : github}
+                    text="github.com/AnastasiaAdamoudi"
+                  />
+                  <ContactLink
+                    href="https://www.linkedin.com/in/anastasiaadamoudi89/"
+                    target="_blank"
+                    rel="noreferrer"
+                    onMouseEnter={handleLinkedinMouseEnter}
+                    onMouseLeave={handleLinkedinMouseLeave}
+                    icon={linkedinIsHovered ? linkedin2 : linkedin}
+                    text="linkedin.com/in/anastasiaadamoudi89"
+                  />
+                </div>
+                {/* End of Contact Links */}
+
+              </div>
+              {/* End of Contact Info */}
+            </div>
+            {/* End of Contact Info Container */}
           </div>
+          {/* End of Contact Information */}
+
+          {/* Contact Form */}
+          <div className="text-center w-[60%] md:w-[40%] mx-auto">
+            <p className="text-white font-body text-lg max-w-lg xl:max-w-6xl pb-5">
+              Or send me a message below:
+            </p>
+
+            <form ref={form} onSubmit={sendEmail}>
+              {/* Form Inputs */}
+              <ContactInput type="text" placeholder="Your name" name="from_name" />
+              <ContactInput type="email" placeholder="Your email address" name="user_email" />
+              <ContactInput type="textarea" placeholder="Your message" name="message" />
+
+              {/* Submit Button */}
+              <div className="mb-4">
+                <button
+                  type="submit"
+                  className="bg-secondary hover:bg-primary mt-1 font-links font-bold uppercase text-sm sm:text-md md:text-l lg:text-xl text-background1 py-2 px-4 rounded-md items-center mx-auto"
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
+          {/* End of Contact Form */}
+        </div>
+      </div>
     </section>
   );
 };
+
+// Additional components (helper components)
+const ContactLink = ({ href, onMouseEnter, onMouseLeave, icon, text }) => (
+  <div className="flex justify-center items-center gap-5 pb-3">
+    <a
+      href={href}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      target="_blank"
+      rel="noreferrer"
+      className="text-secondary hover:text-primary flex items-center whitespace-nowrap text-sm sm:text-sm md:text-md lg:text-md xl:text-xl"
+    >
+      <img src={icon} alt={`${text} icon`} className="w-[20px] h-[20px] object-contain" />
+      <p className="ml-2">{text}</p>
+    </a>
+  </div>
+);
+
+const ContactInput = ({ type, placeholder, name }) => (
+  <div className="mb-4 flex items-center bg-gradient-to-b from-cyan-500 to-purple-300 p-[2px] rounded-lg shadow-card">
+    {type === 'textarea' ? (
+      <textarea
+        name={name}
+        placeholder={placeholder}
+        className="border py-2 px-3 text-body bg-background1 w-full md:w-full rounded-lg text-white text-body"
+      />
+    ) : (
+      <input
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        className="border py-2 px-3 text-body bg-background1 w-full md:w-full rounded-lg text-white text-body"
+      />
+    )}
+  </div>
+);
 
 export default Contact;
