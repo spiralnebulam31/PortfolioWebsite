2b7f2b93f806676649aa3150d9f42c54d2fa4ee4
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-11-16 17:10:27 +0000

    Parallax effect ready for xl screens

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,35 +1,77 @@
-import { motion } from 'framer-motion';
-import { fadeIn } from '../utils/motion.js';
-import { mountains, planet1, planet2, starrySky1 } from '../assets';
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { fadeIn } from "../utils/motion.js";
+import { mountains, planet1, planet2, starrySky1, starrySky2 } from "../assets";
 
 const Parallax = () => {
-    return (
-        <section id="parallax">
-        <div className="bg-gradient-to-b from-background1 to-orange-700 relative z-2 bg-cover w-full left-0 right-0 bottom-0 top-0 py-10 h-full"> 
-            <div className="flex flex-col justify-center gap-5 mb-2 text-center w-full xl:max-w-[40%] mx-auto">
-              <motion.div
-                variants={fadeIn("", "", 0.1, 1)} //direction, type, delay, duration
-                className={
-                  "text-white font-body text-lg max-w-lg xl:max-w-6xl sm:px-16 px-6 py-8 leading-[30px]"
-                }
-              >
-          Let's connect and embark on a collective journey through the ever-evolving tech landscape, charting a course for innovation and growth! 🚀
+
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"]
+  });
+
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "600%"]);
+
+  return (
+    <section id="parallax">
+      <div
+        ref={ref}
+      className="bg-gradient-to-b from-background1 to-orange-700 relative bg-cover w-full left-0 right-0 bottom-0 top-0 pt-10 pb-20 min-h-screen">
+        <div className="flex flex-col justify-center gap-5 mb-2 text-center w-full xl:max-w-[40%] mx-auto">
+          <motion.div
+            style={{ y: yText }}
+            className="text-white font-body text-xl max-w-lg xl:max-w-7xl sm:px-16 px-6 mb-1 mt-[10%] leading-[30px] overflow-hidden"
+          >
+            Let's embark on a journey through the
+            ever-evolving tech landscape, charting a course for innovation and
+            growth!
           </motion.div>
-            </div>
+        </div>
 
-        <div className="bottom-0 left-0 right-0">
-            <img src={mountains} alt="mountains" className="w-full h-full object-cover" />
-        </div>
-        {/* <div className="bottom-0 left-0 right-0">
-            <img src={planet1} alt="planet1" className="w-[300px] h-[300px] object-contain" />
-        </div>
-        <div className="bottom-0 left-0 right-0">
-            <img src={planet2} alt="planet2" className="w-[300px] h-[300px] object-contain" />
-        </div> */}
+            <motion.img
+              src={starrySky2}
+              alt="Starry background"
+              style={{ x: yBg }}
+              className="absolute inset-0 w-full h-full flex justify-center items-center object-cover pointer-events-none z-0 opacity-40"
+            />
 
+            <motion.img
+              src={starrySky1}
+              alt="Starry background"
+              style={{ x: yBg }}
+              className="absolute inset-0 w-full h-full flex justify-center items-center object-cover pointer-events-none z-0 opacity-40"
+            />
+
+        <motion.img
+          src={mountains}
+          alt="mountains"
+          className="absolute w-full h-full object-cover"
+        />
+        <div className="grid grid-cols-2">
+          <motion.div className="flex justify-center items-center">
+            <motion.img
+              src={planet1}
+              alt="planet1"
+              style={{ y: yBg }}
+              className="absolute w-[180px] h-[180px] object-contain z-10 overflow-hidden"
+            />
+          </motion.div>
+
+          <motion.div className="flex justify-center items-center">
+            <motion.img
+              src={planet2}
+              alt="planet2"
+              style={{ y: yBg }}
+              className="absolute w-[80px] h-[80px] object-contain z-10 overflow-hidden"
+            />
+          </motion.div>
         </div>
-        </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
 export default Parallax;
